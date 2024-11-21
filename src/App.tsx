b00ade74974b75e2e08f04271d6b527de9391a62
zf <18927117334@163.com>
2024-11-22 00:23:11 +0800

    feat:run prettierc

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Suspense } from 'react'
+import EditPage from '@/views/editPage.tsx'
+
 function App() {
-  return (
-    <>
-     xixi
-    </>
-  )
+	return (
+		<div className="App">
+			<Suspense fallback="loading">
+				<div className="main">
+					<EditPage />
+				</div>
+			</Suspense>
+		</div>
+	)
 }
 
 export default App
